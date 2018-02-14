fb906a368b97d1f576f143e8bd7922a663cbef1a
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-02-14 07:26:54 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board
+  cell = "   "
+  row = "#{cell}|#{cell}|#{cell}"
+  dash = "-----------"
+  puts row
+  puts dash
+  puts row
+  puts dash
+  puts row
+end
+
