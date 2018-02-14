47c2d37b58625f1a8631a179661d22ffa01fb166
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-02-14 07:42:24 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,9 +7,9 @@ def display_board(board)
   dash = "-----------"
   puts "#{board[0]} | #{board[1]} | #{board[2]}"
   puts dash
-  puts row
+  puts "#{board[3]} | #{board[4]} | #{board[5]}"
   puts dash
-  puts row
+  puts "#{board[6]} | #{board[7]} | #{board[8]}"
 
 
 
