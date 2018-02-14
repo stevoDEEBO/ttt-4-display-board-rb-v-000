79a1df5daab33fbc6de9d771074fc0fc3e5e7e09
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-02-14 07:36:46 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = board = [" "," "," "," "," "," "," "," "," "]
+board = [" "," "," "," "," X "," "," "," "," "]
 def display_board(board)
   cell = "   "
   row = "#{cell}|#{cell}|#{cell}"
